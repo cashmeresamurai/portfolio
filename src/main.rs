f43c5596f5624a61485d5770d4ce4f0b82c11dfc
mur1chan <126001665+mur1chan@users.noreply.github.com>
2024-07-08 14:02:41 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,7 @@ use askama_rocket::Template;
 #[macro_use]
 extern crate rocket;
 
-use include_dir::{include_dir, Dir};
-use rocket::fs::NamedFile;
-use std::path::{Path, PathBuf};
-
-// Einbinden des Verzeichnisses zur Kompilierzeit
-static STATIC_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/static");
+// use rocket::fs::{FileServer, relative};
 
 #[derive(Template)]
 #[template(path = "hello.html")]
@@ -20,15 +15,9 @@ fn hello() -> HelloTemplate<'static> {
     HelloTemplate { name: "world" }
 }
 
-// Handler für statische Dateien
-#[rocket::get("/static/<file..>")]
-async fn static_files(file: PathBuf) -> Option<NamedFile> {
-    let path = Path::new("static").join(&file);
-    NamedFile::open(path).await.ok()
-}
-
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![hello, static_files])
+        .mount("/", routes![hello])
+        // .mount("/static", FileServer::from(relative!("static/")))
 }
