5ba28e70f4b8971937990becbef402d8d6267e6c
mur1chan <126001665+mur1chan@users.noreply.github.com>
2024-07-08 16:15:43 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,35 @@ use askama_rocket::Template;
 #[macro_use]
 extern crate rocket;
 
-use rocket::fs::{relative, FileServer};
 use include_dir::{include_dir, Dir};
-use std::path::{Path, PathBuf};
-use rocket::fs::NamedFile;
+use rocket::http::ContentType;
+use std::path::PathBuf;
+
 #[derive(Template)]
 #[template(path = "hello.html")]
 struct HelloTemplate<'a> {
     name: &'a str,
 }
 
-static STATIC_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/static");
 #[rocket::get("/")]
 fn hello() -> HelloTemplate<'static> {
     HelloTemplate { name: "world" }
 }
 
+static PROJECT_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/static");
+
 // Handler für statische Dateien
-#[rocket::get("/static/<file..>")]
-async fn static_files(file: PathBuf) -> Option<NamedFile> {
-    let path = Path::new("static").join(&file);
-    NamedFile::open(path).await.ok()
+#[rocket::get("/static/<path..>", rank = 100)]
+async fn static_files(path: PathBuf) -> Option<(ContentType, Vec<u8>)> {
+    let path_str = path.to_string_lossy();
+    let data = PROJECT_DIR.get_file(&*path_str)?;
+
+    let content_type = path.extension()
+        .and_then(|e| e.to_str())
+        .and_then(ContentType::from_extension)
+        .unwrap_or(ContentType::Binary);
+
+    Some((content_type, data.contents().to_vec()))
 }
 
 #[launch]
@@ -33,6 +41,5 @@ fn rocket() -> _ {
                 .merge(("address", "0.0.0.0"))
                 .merge(("port", 3949)),
         )
-        .mount("/", routes![hello])
-        .mount("/static", FileServer::from(relative!("static/")))
+        .mount("/", routes![hello, static_files])
 }
