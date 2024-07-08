f1ce3c21280a450e7051b8ada15d6b1638549010
mur1chan <126001665+mur1chan@users.noreply.github.com>
2024-07-08 13:14:29 +0200

    publish test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use askama_rocket::Template;
+#[macro_use]
+extern crate rocket;
+
+use rocket::fs::{FileServer, relative};
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate<'a> {
+    name: &'a str,
+}
+
+#[rocket::get("/")]
+fn hello() -> HelloTemplate<'static> {
+    HelloTemplate { name: "world" }
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![hello])
+        .mount("/static", FileServer::from(relative!("/static/")))
 }
