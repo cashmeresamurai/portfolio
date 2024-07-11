46eeb36d2deefe3e14b0b7d829d31aba0bcc3d3c
mur1chan <126001665+mur1chan@users.noreply.github.com>
2024-07-11 19:11:55 +0200

    fix api request

--- src/db_query.rs
+++ src/db_query.rs
@@ -11,7 +11,7 @@ pub struct AboutMe {
 pub async fn fetch_about_me() -> Result<Vec<AboutMe>> {
     let client = Client::new();
     let res = client
-        .get("http://xswk04c.95.217.222.184.sslip.io")
+        .get("http://xswk04c.95.217.222.184.sslip.io/records")
         .send()
         .await?
         .json::<Vec<AboutMe>>()
