19a3b500c1e1328a3c65ab353f97bdf0d1b74bc1
cashmeresamurai <126001665+cashmeresamurai@users.noreply.github.com>
2025-02-03 22:06:56 +0100

    Update main.rs
    ddd

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,10 @@
 extern crate rocket;
 mod templating;
 use templating::{
-    about_me, index, project_details, projects, return_matrix_client, return_matrix_server,
+    about_me, editor, index, project_details, projects, return_matrix_client, return_matrix_server,
     static_files, submit_task,
 };
 mod pocketbase;
-use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::Header;
-use rocket::{Request, Response};
-
-pub struct CORS;
-
-#[rocket::async_trait]
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "Add CORS headers",
-            kind: Kind::Response,
-        }
-    }
-
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "GET, OPTIONS"));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        // response.set_header(Header::new("Content-Type", "text/html; charset=utf-8"));
-        // X-Frame-Options entfernen oder anpassen
-        response.remove_header("X-Frame-Options");
-
-        // CSP für Embedding erlauben
-        response.set_header(Header::new(
-            "Content-Security-Policy",
-            "frame-ancestors 'self' *;",
-        ));
-    }
-}
 
 #[launch]
 fn rocket() -> _ {
@@ -45,7 +15,6 @@ fn rocket() -> _ {
                 .merge(("address", "0.0.0.0"))
                 .merge(("port", 3949)),
         )
-        .attach(CORS)
         .mount(
             "/",
             routes![
@@ -54,6 +23,7 @@ fn rocket() -> _ {
                 static_files,
                 projects,
                 project_details,
+                // editor,
                 submit_task,
                 return_matrix_server,
                 return_matrix_client
